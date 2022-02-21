6070f2bf279e6116cb97809ffcb1fca151b1a5f3
Tongze Wang <wtongze@gmail.com>
2022-02-21 19:12:52 +0800

    Add more test cases

--- src/common/linux.cpp
+++ src/common/linux.cpp
@@ -14,43 +14,49 @@ Napi::Array getProcesses(const Napi::CallbackInfo& info) {
   Napi::Array processes = Napi::Array::New(env);
   uint32_t index = 0;
 
-  for (auto& item : std::filesystem::directory_iterator("/proc")) {
-    if (item.is_directory()) {
-      std::string fileName{item.path().filename().c_str()};
-      if (std::regex_match(fileName, std::regex{"^\\d+$"})) {
-        pid_t pid = std::stoi(fileName);
-        pid_t ppid = -1;
-        std::string comm;
+  try {
+    for (auto& item : std::filesystem::directory_iterator("/proc")) {
+      if (item.is_directory()) {
+        std::string fileName{item.path().filename().c_str()};
+        if (std::regex_match(fileName, std::regex{"^\\d+$"})) {
+          pid_t pid = std::stoi(fileName);
+          pid_t ppid = -1;
+          std::string comm;
 
-        std::string itemPath{item.path()};
+          std::string itemPath{item.path()};
 
-        std::ifstream commFile{itemPath + "/comm"};
-        commFile >> comm;
+          std::ifstream commFile{itemPath + "/comm"};
+          commFile >> comm;
 
-        std::ifstream stat{itemPath + "/stat"};
+          std::ifstream stat{itemPath + "/stat"};
 
-        std::string rawArgs;
-        std::getline(stat, rawArgs);
-        std::vector<std::string> args;
+          std::string rawArgs;
+          std::getline(stat, rawArgs);
+          std::vector<std::string> args;
 
-        size_t pos = 0;
-        while ((pos = rawArgs.find(' ')) != std::string::npos) {
-          args.push_back(rawArgs.substr(0, pos));
-          rawArgs.erase(0, pos + 1);
+          size_t pos = 0;
+          while ((pos = rawArgs.find(' ')) != std::string::npos) {
+            args.push_back(rawArgs.substr(0, pos));
+            rawArgs.erase(0, pos + 1);
+          }
+          try {
+            ppid = std::stoi(args[3]);
+          } catch (std::exception& e) {
+          }
+
+          Napi::Object obj = Napi::Object::New(env);
+          obj.Set("pid", Napi::Number::New(env, pid));
+          obj.Set("ppid",
+                  ppid == -1 ? env.Undefined() : Napi::Number::New(env, ppid));
+          obj.Set("name", comm.empty() ? env.Undefined()
+                                       : Napi::String::New(env, comm));
+          processes.Set(index, obj);
+          index++;
         }
-        try {
-          ppid = std::stoi(args[3]);
-        } catch (std::exception& e) {
-        }
-
-        Napi::Object obj = Napi::Object::New(env);
-        obj.Set("pid", Napi::Number::New(env, pid));
-        obj.Set("ppid", ppid == -1 ? env.Undefined() : Napi::Number::New(env, ppid));
-        obj.Set("name", comm.empty() ? env.Undefined() : Napi::String::New(env, comm));
-        processes.Set(index, obj);
-        index++;
       }
     }
+  } catch (std::exception& e) {
+    Napi::TypeError::New(env, e.what()).ThrowAsJavaScriptException();
   }
   return processes;
 }
--- test/test.js
+++ test/test.js
@@ -1,10 +1,43 @@
 const memorykit = require('../lib/binding');
+const process = require('process');
 const assert = require('chai').assert;
-const { test } = require('mocha');
 
-test('Return list of processes', () => {
-  const list = memorykit.getProcesses();
-  assert.isAbove(list.length, 0);
+describe('getProcesses()', function () {
+  it('Exports such function', function () {
+    assert.exists(memorykit.getProcesses);
+  });
+  it('Return list of processes', function () {
+    const list = memorykit.getProcesses();
+    assert.isAbove(list.length, 0);
+  });
+});
+
+describe('class Process', function () {
+  it('Exports such class', function () {
+    assert.exists(memorykit.Process);
+  });
+
+  it('Create Process by PID', function () {
+    const current = new memorykit.Process(process.pid);
+    assert.exists(current);
+  });
+
+  it("Can't create Process by string", function () {
+    assert.throw(function () {
+      new memorykit.Process('aaaa');
+    });
+  });
+
+  it("Can't create Process if PID not found", function () {
+    assert.throw(function () {
+      new memorykit.Process(-1024);
+    });
+  });
+
+  const proc = new memorykit.Process(process.pid);
+  it('Has getBaseAddr()', function () {
+    assert.exists(proc.baseAddr);
+  })
 });
 
 // const code = new memorykit.Process(12689);
